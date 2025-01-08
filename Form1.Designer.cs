06a09c4cf803075b1a59d7e6fa0055baa0251e61
rootAngeeeel <juan_vargas@autorefax.com>
2025-01-07 18:37:03 -0600

    changes add some controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,18 @@
             textBox1 = new TextBox();
             label1 = new Label();
             label2 = new Label();
+            label3 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label4 = new Label();
+            colorDialog1 = new ColorDialog();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // cmbx_Errorlvl
@@ -67,17 +79,85 @@
             label2.TabIndex = 3;
             label2.Text = "Url:";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Tamaño del QR:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 133);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(34, 23);
+            numericUpDown1.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(71, 133);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(34, 23);
+            numericUpDown2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(52, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(13, 15);
+            label4.TabIndex = 7;
+            label4.Text = "x";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(421, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(477, 437);
+            panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(22, 15);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(432, 408);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 40);
+            button1.TabIndex = 9;
+            button1.Text = "Selecciona un color";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(910, 548);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(cmbx_Errorlvl);
             Name = "Form1";
             Text = "QR Generator";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -88,5 +168,13 @@
         private TextBox textBox1;
         private Label label1;
         private Label label2;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label4;
+        private ColorDialog colorDialog1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,5 +10,20 @@ namespace QRGeneratorForms
 
             cmbx_Errorlvl.DataSource = Enum.GetValues(typeof(ErrorLevelCorrectionEnum));
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = textBox1.ForeColor;
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                textBox1.ForeColor = MyDialog.Color;
+        }
     }
 }
